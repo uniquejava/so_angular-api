5ecd7a0d1a75c37e124708db3ba4bd351ffef253
cyper <345343747@qq.com>
2016-08-09 16:12:36 +0800

    uh,live template shines here

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/node-api');
 
-var $Entity$ = require('./app/models/$entity$');
+var Car = require('./app/models/car');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || $port$;
+var port = process.env.PORT || 8080;
 
 var router = express.Router();
 router.use(require('./app/utils/cors'));
@@ -25,57 +25,57 @@ router.get('/', function (req, res) {
 });
 
 
-router.route('/$entity$s')
+router.route('/cars')
     .post(function (req, res) {
         console.log(req.body);
-        var $entity$ = req.body;
-        $Entity$.create($entity$, function (err) {
+        var car = req.body;
+        Car.create(car, function (err) {
             if (err) {
                 res.send(err);
             }
 
-            res.json({message: '$Entity$ created!'});
+            res.json({message: 'Car created!'});
         });
     }).get(function (req, res) {
-    $Entity$.find(function (err, $entity$s) {
+    Car.find(function (err, cars) {
         if (err) {
             res.send(err);
         }
 
-        res.json($entity$s);
+        res.json(cars);
     });
 });
 
-router.route('/$entity$s/:$entity$_id')
+router.route('/cars/:car_id')
     .get(function (req, res) {
-        $Entity$.findOne({id: req.params.$entity$_id}, function (err, $entity$) {
+        Car.findOne({id: req.params.car_id}, function (err, car) {
             if (err) {
                 res.send(err);
             }
 
-            res.json($entity$);
+            res.json(car);
         });
 
     })
     .put(function (req, res) {
-        $Entity$.findOne({id: req.params.$entity$_id}, function (err, $entity$) {
+        Car.findOne({id: req.params.car_id}, function (err, car) {
             if (err) {
                 res.send(err);
             }
 
-            $entity$.color = req.body.color;
+            car.color = req.body.color;
 
-            $entity$.save(function (err) {
+            car.save(function (err) {
                 if (err) {
                     res.send(err);
                 }
 
-                res.json({message: '$Entity$ updated!'});
+                res.json({message: 'Car updated!'});
             });
         });
     })
     .delete(function (req, res) {
-        $Entity$.findOneAndRemove({id: req.params.$entity$_id}, function (err, $entity$) {
+        Car.findOneAndRemove({id: req.params.car_id}, function (err, car) {
             if (err) {
                 res.send(err);
             }
--- app/models/car.js
+++ app/models/car.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var $Entity$Schema = new Schema({
-  id: Number,
-  plate: String,
-  color: String,
-  entrance: { type: Date, default: Date.now }
+var CarSchema = new Schema({
+    id: Number,
+    plate: String,
+    color: String,
+    entrance: {type: Date, default: Date.now}
 
 });
 
-module.exports = mongoose.model('$Entity$', $Entity$Schema);
+module.exports = mongoose.model('Car', CarSchema);
